293f6b57bcb316b193a47ad7dac1b7af3f37cce9
Konynnie <konny.zhang@ccc.ac.nz>
2026-05-25 13:29:00 +1200

    Initialize main.jsx with ReactDOM rendering

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CommissionCalculatorApp from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CommissionCalculatorApp />
+  </React.StrictMode>
+);
